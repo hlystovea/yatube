4c4d973cda8c8ef55b3809463244bc306ac363ca
Evgeniy Khlystov <hlystovea@gmail.com>
2021-01-17 15:38:20 +0700

    removed django.extensions

--- yatube/settings.py
+++ yatube/settings.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = [
     'users',
     'about',
     'sorl.thumbnail',
-    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
